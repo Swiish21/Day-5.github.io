0676bbd8c0a9a9bdddd8b6c9c9427917381fb7f0
Swiiish21 <malcolmotieno@gmail.com>
2024-07-17 12:21:41 +0300

    added comments and made minor changes to the html file

--- auth.js
+++ auth.js
@@ -1,4 +1,8 @@
+//below is a sample firebase config that can be used for local development and testing 
 // require('dotenv').config();
+//we are importing firebase modules that we need for the web app
+//they are initializeapp for initialising the firebase app and getdatabase for accessing the database
+//and getauth for accessing the user authentication module
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import {
@@ -6,9 +10,10 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+//This configuration object is used to initialize the Firebase SDK in a web application. By providing these details, you are essentially telling the Firebase SDK how to connect to your specific Firebase project and its resources.
 const firebaseConfig = {
     apiKey: "AIzaSyB-SkwO6gHjjCSXnkOisgorDcY1RlF-xk8",
     authDomain: "demo1-3d336.firebaseapp.com",
@@ -19,16 +24,18 @@ const firebaseConfig = {
     measurementId: "G-KBMQFDBJ9F"
   };
 
+  //below we are getting the initialize app, getting the database and authentication modules that we imported and declaring some variables that we will use later in the code.
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth();
 
-const submitButton = document.getElementById("submit");
+//below we are fetching some elements(id names in green) from the html page(signup.html) where a user can login or register,
+const main = document.getElementById("main");
+const returnBtn = document.getElementById("return-btn");
 
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
-const main = document.getElementById("main");
-const createacct = document.getElementById("create-acct");
+const submitButton = document.getElementById("submit");
 
 const signupEmailIn = document.getElementById("email-signup");
 const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
@@ -36,12 +43,14 @@ const signupPasswordIn = document.getElementById("password-signup");
 const confirmSignUpPasswordIn = document.getElementById(
   "confirm-password-signup"
 );
+//below we are creating a function that will be called when the user clicks on the "Create Account" button.
+const createacct = document.getElementById("create-acct");
 
 // Start with this
 const createacctbtn = document.getElementById("create-acct-btn");
-const returnBtn = document.getElementById("return-btn");
 const signupButton = document.getElementById("sign-up");
 
+//below we are declaring some variables that will be used later to verify the user's email and password if already signed up or create a new account if not.
 var email,
   password,
   signupEmail,
@@ -49,23 +58,25 @@ var email,
   confirmSignupEmail,
   confirmSignUpPassword;
 
+//below is the event listener that will be called when the user clicks on the "Create Account" button.
 createacctbtn.addEventListener("click", function () {
   var isVerified = true;
 
+  //below we are checking if the user's email and password fields are not empty and if they match.
   signupEmail = signupEmailIn.value;
   confirmSignupEmail = confirmSignupEmailIn.value;
   if (signupEmail != confirmSignupEmail) {
     window.alert("Email fields do not match. Try again.");
     isVerified = false;
   }
-
+  //below we are checking if the user's password and confirm password fields match in the new user signup process.
   signupPassword = signupPasswordIn.value;
   confirmSignUpPassword = confirmSignUpPasswordIn.value;
   if (signupPassword != confirmSignUpPassword) {
     window.alert("Password fields do not match. Try again.");
     isVerified = false;
   }
-
+  //below we are checking if the user's email and password fields are not empty, if they are empty or if they do not match we will alert the user to fill out all required fields.
   if (
     signupEmail == null ||
     confirmSignupEmail == null ||
@@ -75,7 +86,7 @@ createacctbtn.addEventListener("click", function () {
     window.alert("Please fill out all required fields.");
     isVerified = false;
   }
-
+  // This code is a part of a signup process where the user's email and password are verified before creating the account. If the isVerified condition is true, it creates a user account using the provided email and password through createUserWithEmailAndPassword method.
   if (isVerified) {
     createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
       .then((userCredential) => {
@@ -83,7 +94,7 @@ createacctbtn.addEventListener("click", function () {
         const user = userCredential.user;
         // ...
         window.alert("Success! Account created.");
-        window.location = "./destinations/destinations.html";
+        window.location = "index.html";
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -95,6 +106,7 @@ createacctbtn.addEventListener("click", function () {
   }
 });
 
+  //Overall, this code snippet demonstrates a basic implementation of user sign-in using Firebase Authentication with error handling and redirection to the index.html page upon successful login.
 submitButton.addEventListener("click", function () {
   email = emailInput.value;
   console.log(email);
@@ -107,7 +119,7 @@ submitButton.addEventListener("click", function () {
       const user = userCredential.user;
 
       window.alert("Success! Welcome back!");
-      window.location = "./destinations/destinations.html";
+      window.location = "index.html";
 
       // ...
     })
@@ -118,8 +130,7 @@ submitButton.addEventListener("click", function () {
     });
 });
 
-// Navigate through the login / Register components
-
+// //This code snippet below adds event listeners to two buttons: `signupButton` and `returnBtn`. When the `signupButton` is clicked, it hides the `main` element and displays the `createacct` element. This is typically used to switch the view from the main page to a signup form. When the `returnBtn` is clicked, it hides the `createacct` element and displays the `main` element. This is typically used to switch the view back to the main page.
 signupButton.addEventListener("click", () => {
   main.style.display = "none";
   createacct.style.display = "block";
