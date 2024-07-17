9644c60ebbb594c743c81fa01002e05e6e71af41
Swiiish21 <malcolmotieno@gmail.com>
2024-07-17 09:42:16 +0300

    added auth.js

---
+++ auth.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB-SkwO6gHjjCSXnkOisgorDcY1RlF-xk8",
+    authDomain: "demo1-3d336.firebaseapp.com",
+    projectId: "demo1-3d336",
+    storageBucket: "demo1-3d336.appspot.com",
+    messagingSenderId: "265419533955",
+    appId: "1:265419533955:web:474e84576ae3fe0bf535b6",
+    measurementId: "G-KBMQFDBJ9F"
+  };
